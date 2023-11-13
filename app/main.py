d2f7339840baaa12ecb6ea01b1000d80809b7de1
Heberto Mayorquin <h.mayorquin@gmail.com>
2023-11-13 12:19:24 +0100

    telemetry working locally

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from typing import List, Dict, Literal, Optional
 import os
+import pyodbc
 
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException, Query, Depends, BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from openai.error import InvalidRequestError
@@ -53,12 +54,40 @@ app.add_middleware(
 )
 
 
+def log_query(text: str, payload_list: List[Payload], ontology: str):
+
+    # Connection string
+    server = os.environ["LOG_DATA_BASE_SERVER"]
+    database = os.environ["LOG_DATA_BASE_NAME"]
+    username = os.environ["LOG_DATA_BASE_USER_NAME"]
+    password = os.environ["LOG_DATA_BASE_USER_PASSWORD"]
+
+    # Connection string
+    database_url = (
+        f"DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}"
+    )
+    with pyodbc.connect(database_url, autocommit=True) as db:
+        try:
+            cursor = db.cursor()
+            query_text = text
+            response_payload = str(payload_list)
+            insert_query = """
+                INSERT INTO QueryLogs (QueryText, Timestamp, Endpoint, ResponsePayload, AdditionalInfo , BackEndLog , OntologyQueried)
+                VALUES (?, GETDATE(), '/get_ontology_matches/', ?, NULL, NULL, ?)
+            """
+            cursor.execute(insert_query, query_text, response_payload, ontology)
+        except Exception as e:
+            print(f"An error occurred while logging: {e}")
+
+
 @app.get("/get_ontology_matches/", response_model=List[Payload])
 async def get_ontology_matches(
+    background_tasks: BackgroundTasks,
     text: str = Query(..., description="Text excerpt to match against"),
-    ontology: Optional[Literal["neuro_behavior_ontology", "cognitiveatlas"]] = Query("neuro_behavior_ontology", description="Ontology type to use"),
+    ontology: Optional[Literal["neuro_behavior_ontology", "cognitiveatlas"]] = Query(
+        "neuro_behavior_ontology", description="Ontology type to use"
+    ),
 ):
-
     api_key = os.environ.get("QDRANT_API_KEY")
     if not api_key:
         raise ValueError("QDRANT_API_KEY environment variable not set")
@@ -74,7 +103,7 @@ async def get_ontology_matches(
         results_list = semantic_match(text=text, top=top, score_threshold=score_threshold, ontology=ontology)
         if ontology in ["neuro_behavior_ontology"]:
             results_list = rerank(results_list, text, ontology=ontology)
-        
+
     except InvalidRequestError as e:
         # Handle the specific InvalidRequestError
         detail = "Text too large. Try using a shorter description or use smaller pieces of your text as input."
@@ -83,11 +112,14 @@ async def get_ontology_matches(
     except Exception as e:
         # Handle general errors and raise a 500 Internal Server Error
         raise HTTPException(status_code=500, detail=str(e))
-    
+
     if results_list:
         payload_list = [result.payload for result in results_list][:5]
-        
+
     else:
         payload_list = []
+
+    # loging
+    background_tasks.add_task(log_query, text, payload_list, ontology)
 
     return payload_list
